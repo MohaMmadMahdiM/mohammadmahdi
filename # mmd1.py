90181d18dc63bab924c7d4f7f462c68e3f1f7f63
MohaMmadMahdiM <157895248+MohaMmadMahdiM@users.noreply.github.com>
2024-01-27 17:31:38 +0330

    Add files via upload

---
+++ # mmd1.py
@@ -0,0 +1,82 @@
+# mmd1
+
+
+# metr be santi metr
+
+from tkinter import *
+
+root=Tk()
+
+root.geometry("500x300+480+250")
+
+lab1=Label(root,text="عدد کاربر به متر:")
+lab1.grid(row=0,column=0)
+
+lab2=Label(root,text="عدد به سانتی متر")
+lab2.grid(row=3,column=0)
+
+
+ent=Entry(root)
+ent.grid(row=0,column=1)
+
+def mmd1():
+    m=int(ent.get())*100
+    tex.insert(INSERT,m)
+
+
+but=Button(text="تایید",command=mmd1)
+but.grid(row=0,column=3)
+
+tex=Text(root,bg="green",height=5,width=25)
+tex.grid(row=4,column=0)
+
+
+root.mainloop()
+
+
+
+
+
+# kg be g
+
+from tkinter import *
+
+root=Tk()
+
+root.geometry("500x300+480+250")
+
+lab1=Label(root,text="عدد کاربر به کیلو گرم:")
+lab1.grid(row=0,column=0)
+
+lab2=Label(root,text="عدد به گرم")
+lab2.grid(row=3,column=0)
+
+
+ent=Entry(root)
+ent.grid(row=0,column=1)
+
+def mmd1():
+    km=int(ent.get())*1000
+    tex.insert(INSERT,km)
+
+
+but=Button(text="تایید",command=mmd1)
+but.grid(row=0,column=3)
+
+tex=Text(root,bg="#47FFDB",height=5,width=25)
+tex.grid(row=4,column=0)
+
+
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
